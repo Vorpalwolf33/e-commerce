ba5abc7e8fe4706efd2daa2c8aa75eb25ebf75e7
Shamanth <shamanthkumar33@gmail.com>
2020-05-14 15:45:04 +0530

    Added all the dependencies of the backend

--- config/configDB.js
+++ config/configDB.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const configDB = () => {
     mongoose.Promise = global.Promise;
 
-    mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost:27017/ecommerce", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+    mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost:27017/ecommerce", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
         .then( () => {console.log("Succesfully connected to database")})
         .catch( () => {console.log("Error while connecting to database: \n", err)})
 }
 
-module.exports = configDB;
+module.exports = configDB;  
--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const routes = express.Router();
+const {authenticateUser, authoriseUser} = require('../app/middlewares/authentication');
 
-routes.get('/', (req, res) => { res.json("Message from backend");})
+const userControllers = require('../app/controllers/userControllers');
+const productControllers = require('../app/controllers/productControllers');
+const categoryControllers = require('../app/controllers/categoryController');
+const orderControllers = require('../app/controllers/orderControllers');
+
+routes.post('/register', userControllers.register);
+routes.post('/login', userControllers.login);
+routes.get('/account/home', authenticateUser, userControllers.home);
+routes.get('/logout', authenticateUser, userControllers.logout);
+routes.get('/', userControllers.home);
+routes.post('/account/product/add', authoriseUser, productControllers.add);
+routes.post('/account/category/add', authoriseUser, categoryControllers.add);
+routes.get('/account/categories', authenticateUser, categoryControllers.show);
+routes.get('/account/products', authoriseUser, productControllers.list);
+routes.get('/guest/products', productControllers.list);
+routes.get('/product/:id', productControllers.show);
+routes.post('/account/orders/add', authenticateUser, orderControllers.add);
+routes.post('/account/cart/add', authenticateUser, userControllers.addToCart);
 
 module.exports = routes;
