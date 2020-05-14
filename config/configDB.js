8634941054d0b2f50f6fbeea7f52faba2bfd3e76
Shamanth <shamanthkumar33@gmail.com>
2020-05-14 15:32:41 +0530

    Connected to database and added routes

---
+++ config/configDB.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const configDB = () => {
+    mongoose.Promise = global.Promise;
+
+    mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost:27017/ecommerce", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+        .then( () => {console.log("Succesfully connected to database")})
+        .catch( () => {console.log("Error while connecting to database: \n", err)})
+}
+
+module.exports = configDB;
---
+++ index.js
@@ -0,0 +1,17 @@
+const configDB = require('./config/configDB');
+const routes = require('./config/routes');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3010;
+
+configDB();
+
+app.use(express.json());
+app.use(cors());
+app.use('/', routes);
+
+app.listen(PORT, () => {
+    console.log('listening to port: ', PORT);
+});
