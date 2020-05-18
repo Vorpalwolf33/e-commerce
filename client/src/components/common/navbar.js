1c02c7ecfaaa775c8916d00e81ddb0505ddb3133
Shamanth <shamanthkumar33@gmail.com>
2020-05-18 11:36:13 +0530

    Added different navbars for different type of user

--- client/src/components/common/navbar.js
+++ client/src/components/common/navbar.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import '../../styles/navbar.css';
+import {Route, Switch} from 'react-router-dom';
+
+const GuestNavbar = (props) => {
+    return (
+        <div>
+            Guest Navbar
+        </div>
+    )
+}
+
+const CustomerNavbar = (props) => {
+    return (
+        <div>
+            Customer Navbar
+        </div>
+    )
+}
+
+const AdminNavbar = (props) => {
+    return (
+        <div>
+            Admin Navbar
+        </div>
+    )
+}
 
 export default (props) => {
     return(
         <div>
-            Navbar
+            Navbar:
+            <Switch>
+                <Route path="/account" component={CustomerNavbar} />
+                <Route path="/admin" component={AdminNavbar} />
+                <Route path="/" exact={true} component={GuestNavbar}/>
+            </Switch>
         </div>
     )
 }
