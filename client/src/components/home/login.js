2e82c7485e74aa0bdd5bce7d8d941a774c7afd1d
unknown <shamanthkumar33@gmail.com>
2020-06-14 09:18:15 +0530

    Added input fields to login form

--- client/src/components/home/login.js
+++ client/src/components/home/login.js
@@ -2,16 +2,43 @@ import React from 'react';
 import Navbar from '../common/navbar';
 import {Link} from 'react-router-dom';
 
-export default (props) => {
-    return (
-        <div>
-            <Navbar />
-            Login form
-            <form>
+export default class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
 
-            </form>
-            To create a new account <Link to="/register">Click here</Link>
-            <Link to="/"><button>Back</button></Link>
-        </div>
-    )
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    render() {
+
+        return (
+            <div>
+                <Navbar />
+                Login form
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        <input type="text" placeholder="Email" name="email" value={this.state.email} onChange={this.handleChange} />
+                    </label><br/>
+                    <label>
+                        <input type="password" placeholder="Password" name="password" value={this.state.password} onChange={this.handleChange} />
+                    </label><br/>
+                    <input type="submit" value="login" />
+                </form>
+                To create a new account <Link to="/register">Click here</Link>
+                <Link to="/"><button>Back</button></Link>
+            </div>
+        )
+    }
 }
